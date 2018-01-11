ddfe284659104c735577ee2715143a404f5d7282
yyx <yangyunxin@biosan-screening>
2018-01-11 22:40:24 +0800

    remove

--- src/page/TestUserInfo/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as ReactDOM  from 'react-dom';
-import * as React from 'react';
-import 'antd/dist/antd.css';
-// import Home from './home';
-// import {Page} from './Page';
-import routeConfig from '../../route/route';
-
-ReactDOM.render(routeConfig, document.getElementById('root'));
-
